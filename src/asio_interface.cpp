bb2528fb851c2d40ef63ef44821aeb69f09a1254
Richard Larsson <larsson@mps.mpg.de>
2020-08-14 05:42:41 +0200

    Basic serial port that compiles

--- src/asio_interface.cpp
+++ src/asio_interface.cpp
@@ -1,4 +1,12 @@
 #include "asio_interface.h"
 
 namespace Network {
+class Serial {
+  asio::serial_port ser;
+  
+public:
+  Serial(const std::string& dev, int baudrate) : ser({}, dev) {
+    ser.set_option(asio::serial_port::baud_rate(baudrate));
+  }
+};
 }
