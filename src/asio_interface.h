e752dd1738dccf63fc423cb4338526ed8c5b2ac3
Richard Larsson <ric.larsson@gmail.com>
2020-08-14 13:05:34 +0200

    ASIO tests

--- src/asio_interface.h
+++ src/asio_interface.h
@@ -7,23 +7,24 @@
 
 namespace Network {
 class Serial {
-  asio::serial_port ser;
+  asio::io_service io;
+  asio::serial_port serial;
   
 public:
-  Serial() : ser({}) {
+  Serial() : io(), serial(io) {
   }
   
   void set_baudrate(size_t baudrate) {
-    ser.set_option(asio::serial_port::baud_rate(baudrate));
+    serial.set_option(asio::serial_port::baud_rate(baudrate));
   }
   
-  void open(const std::string& dev) {ser.open(dev);}
+  void open(const std::string& dev) {serial.open(dev);}
   
-  void close() {ser.close();}
+  void close() {serial.close();}
   
   void write(const std::string& data) {
     asio::error_code ec;
-    asio::write(ser, asio::const_buffer(data.data(), data.size()), ec);
+    asio::write(serial, asio::const_buffer(data.data(), data.size()), ec);
     if (ec)
       std::cerr << ec << '\n';
   }
@@ -31,7 +32,7 @@ public:
   std::string readline() {
     std::string data;
     asio::error_code ec;
-    size_t n = asio::read_until(ser, asio::dynamic_buffer(data), '\n', ec);
+    size_t n = asio::read_until(serial, asio::dynamic_buffer(data), '\n', ec);
     if (ec)
       std::cerr << ec << '\n';
     return data.substr(0, n);
